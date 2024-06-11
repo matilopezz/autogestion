755b864484b715b637dcc804f265cea23853e6aa
matilopezz <matilopezo25@gmail.com>
2024-06-11 12:47:38 -0300

    prueba 2

--- src/main/java/com/autogestion1/autogestion/AutogestionApplication.java
+++ src/main/java/com/autogestion1/autogestion/AutogestionApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AutogestionApplication {
 
 
-	//prueba de git
+
 	public static void main(String[] args) {
 		SpringApplication.run(AutogestionApplication.class, args);
 	}
